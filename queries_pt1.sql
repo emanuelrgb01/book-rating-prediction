fc3f2e4080a861ed45f9e86aaa6fdfad26471072
pedro-pinheiro002 <pedropinheiroborges2@gmail.com>
2025-04-13 16:11:29 -0300

    refactor: transform sql command to upper case

--- queries_pt1.sql
+++ queries_pt1.sql
@@ -1,19 +1,19 @@
-select category, count(isbn) as count_book from books group by category order by count_book desc;
+SELECT category, COUNT(isbn) AS count_book FROM books GROUP BY category ORDER BY count_book DESC;
 
-select state, count(user_id) as count_user from users group by state order by count_user desc;
+SELECT state, COUNT(user_id) AS count_user FROM users GROUP BY state ORDER BY count_user DESC;
 
-select isbn, avg(rating) as average_rating from ratings group by isbn order by average_rating desc;
+SELECT isbn, AVG(rating) AS average_rating FROM ratings GROUP BY isbn ORDER BY average_rating DESC;
 
-select b.book_title, avg(r.rating) as average_rating
-from books b join ratings r on b.isbn = r.isbn
-where b.category = E'[\'Biography & Autobiography\']'
-group by b.book_title
-order by average_rating desc;
+SELECT b.book_title, AVG(r.rating) AS average_rating
+FROM books b JOIN ratings r ON b.isbn = r.isbn
+WHERE b.category = E'[\'Biography & Autobiography\']'
+GROUP BY b.book_title
+ORDER BY average_rating DESC;
 
-select b.book_title, avg(r.rating)  as average_rating, count(r.rating) as count_rating
-from books b 
-join ratings r on b.isbn = r.isbn
-join users u on u.user_id = r.user_id
-where u.state = 'catalunya'
-group by b.book_title
-order by count_rating desc;
+SELECT b.book_title, AVG(r.rating)  AS average_rating, COUNT(r.rating) AS count_rating
+FROM books b
+JOIN ratings r ON b.isbn = r.isbn
+JOIN users u ON u.user_id = r.user_id
+WHERE u.state = 'catalunya'
+GROUP BY b.book_title
+ORDER BY count_rating DESC;
